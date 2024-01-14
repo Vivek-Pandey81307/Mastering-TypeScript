7f2671b28e1fd1815e5d34b465052cdd4f428dc1
Vivek Kumar Pandey <vs9595@srmist.edu.in>
2024-01-14 10:16:23 +0530

    object

--- 01/dist/index.js
+++ 01/dist/index.js
@@ -1,11 +1,18 @@
 "use strict";
 // type userName = ( n:number ,m : number )=> void; 
 // const func : userName= (n,m)=>{alert(n*m)}
-// let arr : number[];
-// const arr2 :Array<string>= ["Hello","Sir","Good Morning"]
-// var arr3 : Array<boolean>=new Array(2);
-// arr3 =[true,false,false,true]
-// arr2.forEach(i=>{
-//     alert(i);
-// })
-// const arr : [number,number,number] = [324,234,234];
+// alert(typeof(func(3,4)))
+// // let arr : number[];
+// // const arr2 :Array<string>= ["Hello","Sir","Good Morning"]
+// // var arr3 : Array<boolean>=new Array(2);
+// // arr3 =[true,false,false,true]
+// // arr2.forEach(i=>{
+// //     alert(i);
+// // })
+const obj2 = { name: "chakravartin", age: 34 };
+const myobject = { height: 34, weight: 43 };
+const objfunc = (o1, o2) => {
+    console.log(`o1 = ${JSON.stringify(o1)} end_o1`);
+    console.log(`o2 = ${JSON.stringify(o2)} end_o2`);
+};
+objfunc(myobject, obj2);
--- 01/src/index.ts
+++ 01/src/index.ts
@@ -1,13 +1,23 @@
 // type userName = ( n:number ,m : number )=> void; 
 // const func : userName= (n,m)=>{alert(n*m)}
+// alert(typeof(func(3,4)))
+// // let arr : number[];
+// // const arr2 :Array<string>= ["Hello","Sir","Good Morning"]
+// // var arr3 : Array<boolean>=new Array(2);
+// // arr3 =[true,false,false,true]
+// // arr2.forEach(i=>{
+// //     alert(i);
+// // })
 
-// let arr : number[];
-// const arr2 :Array<string>= ["Hello","Sir","Good Morning"]
-// var arr3 : Array<boolean>=new Array(2);
-// arr3 =[true,false,false,true]
-// arr2.forEach(i=>{
-//     alert(i);
-// })
+// // const arr : [number,number,number] = [324,234,234];
 
+type obj1 = {height : number,weight :number}
+const obj2:{name : string,age:number} = {name : "chakravartin",age:34}
 
-// const arr : [number,number,number] = [324,234,234];
+const myobject : obj1 = {height:34,weight:43}
+
+const objfunc = (o1 :obj1 ,o2 :{name : string,age:number})=>{
+    console.log(`o1 = ${JSON.stringify(o1)} end_o1`);
+    console.log(`o2 = ${JSON.stringify(o2)} end_o2`);
+}
+objfunc(myobject,obj2)
