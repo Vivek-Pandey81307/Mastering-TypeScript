ee8874c649624efe101d6a1c481ea4e93bad899d
Vivek Kumar Pandey <vs9595@srmist.edu.in>
2024-01-14 07:19:58 +0530

    pushing to git

---
+++ 01/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+const func = (n, m) => { alert(n * m); };
+let arr;
+const arr2 = ["Hello", "Sir", "Good Morning"];
+var arr3 = new Array(2);
+arr3 = [true, false, false, true];
+arr2.forEach(i => {
+    alert(i);
+});
---
+++ 01/src/index.ts
@@ -0,0 +1,10 @@
+type userName = ( n:number ,m : number )=> void; 
+const func : userName= (n,m)=>{alert(n*m)}
+
+let arr : number[];
+const arr2 :Array<string>= ["Hello","Sir","Good Morning"]
+var arr3 : Array<boolean>=new Array(2);
+arr3 =[true,false,false,true]
+arr2.forEach(i=>{
+    alert(i);
+})
