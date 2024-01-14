65353cc0426e3f294fe55319487c9428873a7475
Vivek Kumar Pandey <vs9595@srmist.edu.in>
2024-01-14 08:57:05 +0530

    fixed size array

--- 01/dist/index.js
+++ 01/dist/index.js
@@ -1,9 +1,11 @@
 "use strict";
-const func = (n, m) => { alert(n * m); };
-let arr;
-const arr2 = ["Hello", "Sir", "Good Morning"];
-var arr3 = new Array(2);
-arr3 = [true, false, false, true];
-arr2.forEach(i => {
-    alert(i);
-});
+// type userName = ( n:number ,m : number )=> void; 
+// const func : userName= (n,m)=>{alert(n*m)}
+// let arr : number[];
+// const arr2 :Array<string>= ["Hello","Sir","Good Morning"]
+// var arr3 : Array<boolean>=new Array(2);
+// arr3 =[true,false,false,true]
+// arr2.forEach(i=>{
+//     alert(i);
+// })
+// const arr : [number,number,number] = [324,234,234];
--- 01/src/index.ts
+++ 01/src/index.ts
@@ -1,10 +1,13 @@
-type userName = ( n:number ,m : number )=> void; 
-const func : userName= (n,m)=>{alert(n*m)}
+// type userName = ( n:number ,m : number )=> void; 
+// const func : userName= (n,m)=>{alert(n*m)}
 
-let arr : number[];
-const arr2 :Array<string>= ["Hello","Sir","Good Morning"]
-var arr3 : Array<boolean>=new Array(2);
-arr3 =[true,false,false,true]
-arr2.forEach(i=>{
-    alert(i);
-})
+// let arr : number[];
+// const arr2 :Array<string>= ["Hello","Sir","Good Morning"]
+// var arr3 : Array<boolean>=new Array(2);
+// arr3 =[true,false,false,true]
+// arr2.forEach(i=>{
+//     alert(i);
+// })
+
+
+// const arr : [number,number,number] = [324,234,234];
