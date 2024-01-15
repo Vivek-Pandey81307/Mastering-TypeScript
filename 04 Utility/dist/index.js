7fc74a242299e119734f51c11987de603446442f
Vivek Kumar Pandey <vs9595@srmist.edu.in>
2024-01-15 15:52:40 +0530

    Record

--- 04 Utility/dist/index.js
+++ 04 Utility/dist/index.js
@@ -1,8 +1,1 @@
 "use strict";
-// type User2= Partial<User>
-// type User2 = Required<User>
-const user = {
-    name: "abhi",
-    email: "jaihind@gail.com"
-};
-user.name = "narasj";
--- 04 Utility/src/index.ts
+++ 04 Utility/src/index.ts
@@ -1,11 +1,13 @@
 type User = {
-    name ?: string,
-    email ?:string,
+    name : string,
+    email :string,
+    gender : boolean,
 }
 // type User2= Partial<User>
 // type User2 = Required<User>
-const user:Readonly<User>={
-    name :"abhi",
-    email:"jaihind@gail.com"
-}
-user.name="narasj";
+// const user:Readonly<User>={
+//     name :"abhi",
+//     email:"jaihind@gail.com"
+// }
+// user.name="narasj";
+type User2 = Record<"name"|"email"|"gender",string>
