e33597199614dccb38192c01f2b1a46cf75997a8
Vivek Kumar Pandey <vs9595@srmist.edu.in>
2024-01-15 13:52:14 +0530

    indexSignature

--- 03 Type Assertion/dist/index.js
+++ 03 Type Assertion/dist/index.js
@@ -1,21 +1,26 @@
 "use strict";
 // // const btn = document.getElementById("btn") as HTMLElement;
-// // const btn =<HTMLElement> document.getElementById("btn")
-// const btn = document.getElementById("btn")!
-// btn.onclick
-// // const img = document.getElementById("myimg") as HTMLImageElement;
-// // img.src
-// const img = document.querySelector("img")!
-// img.src
-const form = document.getElementById("myform");
-const myinput = document.querySelector("form >input");
-form.onsubmit = (e) => {
-    var _a;
-    e.preventDefault();
-    const value = Number(myinput.value);
-    alert(value);
-    alert(typeof value);
-    const h2 = document.createElement("h2");
-    h2.textContent = String(value + 20);
-    (_a = document.querySelector("body")) === null || _a === void 0 ? void 0 : _a.append(h2);
+const myobj = {
+    name: "Abhi",
+    email: "abhi@gmail.com"
 };
+//keyOfOperator
+// interface Person{
+//   name : string;
+//   email : string;
+// }
+const getName = () => {
+    return myobj.name;
+};
+const getEmail = () => {
+    return myobj.email;
+};
+const getData = (key) => {
+    return myobj[key];
+};
+//keyOfOperator
+// const getData = (key:"name" | "email"):string=>{
+//     return myobj[key];
+// }
+alert(myobj.name);
+alert(getData("email"));
--- 03 Type Assertion/src/index.ts
+++ 03 Type Assertion/src/index.ts
@@ -13,16 +13,47 @@
 // img.src
 
 
-const form = document.getElementById("myform") as HTMLFormElement
-const myinput = document.querySelector("form >input") as HTMLInputElement
-form.onsubmit =(e: SubmitEvent)=>{
-    e.preventDefault();
-    const value = Number(myinput.value);
-    alert(value)
-    alert(typeof value);
-    const h2 = document.createElement("h2");
-    h2.textContent=String(value+20);
-    document.querySelector("body")?.append(h2);
+// const form = document.getElementById("myform") as HTMLFormElement
+// const myinput = document.querySelector("form >input") as HTMLInputElement
+// form.onsubmit =(e: SubmitEvent)=>{
+//     e.preventDefault();
+//     const value = Number(myinput.value);
+//     alert(value)
+//     alert(typeof value);
+//     const h2 = document.createElement("h2");
+//     h2.textContent=String(value+20);
+//     document.querySelector("body")?.append(h2);
+// }
+
+// indexSignature
+interface Person{
+   [key:string]:string;
+}
+
+const myobj : Person = {
+    name : "Abhi",
+    email : "abhi@gmail.com"
+} 
+//keyOfOperator
+// interface Person{
+//   name : string;
+//   email : string;
+// }
+const getName = ():string=>{
+    return myobj.name
+}
+const getEmail = ():string=>{
+    return myobj.email
 }
 
 
+const getData = (key:string):string =>{
+    return myobj[key]
+}
+
+//keyOfOperator
+// const getData = (key:"name" | "email"):string=>{
+//     return myobj[key];
+// }
+alert(myobj.name);
+alert(getData("email"))
