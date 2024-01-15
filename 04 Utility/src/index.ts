24d99ffb93c861742cd664d187849c54b4720383
Vivek Kumar Pandey <vs9595@srmist.edu.in>
2024-01-15 15:41:16 +0530

    Required Utility

--- 04 Utility/src/index.ts
+++ 04 Utility/src/index.ts
@@ -1,5 +1,6 @@
 type User = {
-    name : string,
-    email:string,
+    name ?: string,
+    email ?:string,
 }
-type User2= Partial<User>
+// type User2= Partial<User>
+type User2 = Required<User>
