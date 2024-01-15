b2eff1490586a83d9fa5175550a1baf7d0324173
Vivek Kumar Pandey <vs9595@srmist.edu.in>
2024-01-15 10:30:36 +0530

    Type Assertion

---
+++ 03 Type Assertion/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// // const btn = document.getElementById("btn") as HTMLElement;
+// // const btn =<HTMLElement> document.getElementById("btn")
+// const btn = document.getElementById("btn")!
+// btn.onclick
+// // const img = document.getElementById("myimg") as HTMLImageElement;
+// // img.src
+// const img = document.querySelector("img")!
+// img.src
