44c6857e1d014b05eefca15329243acaa7e5fd8e
Vivek Kumar Pandey <vs9595@srmist.edu.in>
2024-01-15 16:07:58 +0530

    Record with interface

--- 04 Utility/dist/index.js
+++ 04 Utility/dist/index.js
@@ -1,1 +1,20 @@
 "use strict";
+// type User = {
+//     name : string,
+//     email :string,
+//     gender : boolean,
+// }
+// // type User2= Partial<User>
+// // type User2 = Required<User>
+// // const user:Readonly<User>={
+// //     name :"abhi",
+// //     email:"jaihind@gail.com"
+// // }
+// // user.name="narasj";
+// type User2 = Record<"name"|"email"|"gender",string>
+const users = {
+    john: { age: 34 },
+    elon: { age: 43 },
+    andrew: { age: 32 },
+    jack: { age: 23 },
+};
--- 04 Utility/src/index.ts
+++ 04 Utility/src/index.ts
@@ -1,13 +1,28 @@
-type User = {
-    name : string,
-    email :string,
-    gender : boolean,
-}
-// type User2= Partial<User>
-// type User2 = Required<User>
-// const user:Readonly<User>={
-//     name :"abhi",
-//     email:"jaihind@gail.com"
+// type User = {
+//     name : string,
+//     email :string,
+//     gender : boolean,
 // }
-// user.name="narasj";
-type User2 = Record<"name"|"email"|"gender",string>
+// // type User2= Partial<User>
+// // type User2 = Required<User>
+// // const user:Readonly<User>={
+// //     name :"abhi",
+// //     email:"jaihind@gail.com"
+// // }
+// // user.name="narasj";
+// type User2 = Record<"name"|"email"|"gender",string>
+
+
+interface UserInfo{
+    age :number;
+}
+
+type UserName = "john" | "andrew"|"elon"|"jack";
+
+const users:Record<UserName,UserInfo>={
+    john:{age:34},
+    elon:{age:43},
+    andrew:{age:32},
+    jack:{age : 23},
+}
+
