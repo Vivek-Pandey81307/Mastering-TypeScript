dfe31b31d362bb5496dbe4642e5b6ec4b612f513
Vivek Kumar Pandey <vs9595@srmist.edu.in>
2024-01-15 09:38:58 +0530

    class implements interface

--- 02 Classes/dist/index.js
+++ 02 Classes/dist/index.js
@@ -1,29 +1,19 @@
 "use strict";
-class Player {
-    constructor(height, weight, power) {
-        this.height = height;
-        this.weight = weight;
-        this.power = power;
-        this.id = String(Math.random() * 100);
+// class Player{
+//     public readonly id : string;
+//     constructor(
+//         private height : number,
+//         public weight :number,
+//         protected power : number
+class Product {
+    constructor(name, price, stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.id = (Math.random() * 1000);
     }
-    // getMyHeight = ()=>this.height;
-    get getMyHeight() {
-        return this.height;
-    }
-    set changeMyHeight(val) {
-        this.height = val;
-    }
+    get getId() { return this.id; }
+    ;
 }
-// class Player2 extends Player{
-//     special : boolean;
-//     constructor(height :number,weight:number
-//         ,power : number ,special : boolean){
-//             super(height,weight,power);
-//             this.special = special;
-//         }
-//         getMyPower=()=>this.power;
-// }
-const abhi = new Player(100, 230, 34);
-console.log(abhi.getMyHeight);
-abhi.changeMyHeight = 500;
-console.log(abhi.getMyHeight);
+const product1 = new Product("Macbook", 2000, 20);
+alert(product1.getId);
--- 02 Classes/src/index.ts
+++ 02 Classes/src/index.ts
@@ -1,37 +1,58 @@
-class Player{
-    public readonly id : string;
-    constructor(
-        private height : number,
-        public weight :number,
-        protected power : number
+// class Player{
+//     public readonly id : string;
+//     constructor(
+//         private height : number,
+//         public weight :number,
+//         protected power : number
 
-    ){
-        this.id =String(Math.random()*100)
-    }
-    // getMyHeight = ()=>this.height;
+//     ){
+//         this.id =String(Math.random()*100)
+//     }
+//     // getMyHeight = ()=>this.height;
 
-    get getMyHeight():number{
-        return this.height ;
-    }
-    set changeMyHeight(val:number){
-        this.height = val 
-    }
-
-}
-// class Player2 extends Player{
-//     special : boolean;
-//     constructor(height :number,weight:number
-//         ,power : number ,special : boolean){
-//             super(height,weight,power);
-//             this.special = special;
-            
-//         }
-//         getMyPower=()=>this.power;
-
+//     get getMyHeight():number{
+//         return this.height ;
+//     }
+//     set changeMyHeight(val:number){
+//         this.height = val 
+//     }
 
 // }
-const abhi = new Player(100,230,34);
-console.log(abhi.getMyHeight)
-abhi.changeMyHeight=500;
-console.log(abhi.getMyHeight)
+// // class Player2 extends Player{
+// //     special : boolean;
+// //     constructor(height :number,weight:number
+// //         ,power : number ,special : boolean){
+// //             super(height,weight,power);
+// //             this.special = special;
+            
+// //         }
+// //         getMyPower=()=>this.power;
 
+
+// // }
+// const abhi = new Player(100,230,34);
+// console.log(abhi.getMyHeight)
+// abhi.changeMyHeight=500;
+// console.log(abhi.getMyHeight)
+
+interface ProductType{
+    name : string;
+    price : number;
+    stock : number;
+    id : number;
+    offer?:boolean;
+}
+interface GiveId{
+    getId :number,
+}
+class Product implements ProductType,GiveId{
+  
+   public id : number = (Math.random()* 1000);
+   
+   constructor(public name : string, public price :number,public stock :number){
+   
+   }
+   get getId(){return this.id};
+}
+const product1 = new Product ("Macbook",2000,20);
+alert(product1.getId)
