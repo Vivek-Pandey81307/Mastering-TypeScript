abbe3987e7d902ecfc67f34b8284b1300c3f6ec5
Vivek Kumar Pandey <vs9595@srmist.edu.in>
2024-01-16 09:34:16 +0530

    Generic -Completed

--- 05 Generics in TypeScript/dist/index.js
+++ 05 Generics in TypeScript/dist/index.js
@@ -1,18 +1,50 @@
 "use strict";
-// // const func= (n:any):any=>{
-// //     return n;
+// // // const func= (n:any):any=>{
+// // //     return n;
+// // // }
+// // // const ans = func(20);
+// // //generic
+// // // const func = <T>(n:T):T=>{
+// // //     return n;
+// // // }
+// // // const ans = func(20);
+// // // const ans2= func("string");
+// // // const ans3 = func(true);
+// // // type Person = {
+// // //     name :string,
+// // //     age :number
+// // // }
+// // // const func = <T>(n:T):T=>{
+// // //     return n;
+// // // }
+// // // const abhi:Person = {name:"Rahj",age:34}
+// // // const ans = func<Person>(abhi)
+// // // alert(JSON.stringify(ans))
+// // const arr:number[]=[]
+// // const arr2:Array<number>=[]
+// // const func = < T ,U>(n:T ,o:U):{n:T,o:U}=>{
+// //     return {n,o};
 // // }
-// // const ans = func(20);
-const users = [
-    { name: "aghil", age: 34 },
-    { name: "chakiv", age: 32 },
-    { name: "sajjo", age: 23 },
-    { name: "vakilsh", age: 72 }
-];
-const filterByPeoples = (arr, property, value) => {
-    return arr.filter(item => item[property] === value);
-};
-const filteredPeopleByName = filterByPeoples(users, "name", "aghil");
-const filteredPeopleByAge = filterByPeoples(users, "age", 32);
-alert(JSON.stringify(filteredPeopleByName));
-alert(JSON.stringify(filteredPeopleByAge));
+// // // const ans = func<number,string>(20,"lol");
+// // // console.log(ans.n)
+// // type Person={
+// //     name :string,age:number
+// // }
+// // const func = < T , U extends T>(n:T ,o:U):{n:T,o:U}=>{
+// //     return {n,o};
+// // }
+// // const ans = func<Person ,{name:string,age:number,email:string}>({name:"akash",age:34},{name:"sakhal",age:23,email:"chakravartin"});
+// type Person={name:string,age:number}
+// const users:Person[]=[
+//     {name:"aghil",age:34},
+//     {name:"chakiv",age:32},
+//     {name :"sajjo",age:23},
+//     {name:"vakilsh",age:72}
+// ]
+// const filterByPeoples = <T,Key extends keyof T>(arr:T[],property:Key,value:T[Key]):T[]=>{
+//    return  arr.filter(item=>item[property]===value)
+// }
+// const filteredPeopleByName = filterByPeoples(users,"name","aghil")
+// const filteredPeopleByAge = filterByPeoples(users,"age",32)
+// alert(JSON.stringify(filteredPeopleByName))
+// alert(JSON.stringify(filteredPeopleByAge))
--- 05 Generics in TypeScript/src/index.ts
+++ 05 Generics in TypeScript/src/index.ts
@@ -1,60 +1,62 @@
-// // const func= (n:any):any=>{
-// //     return n;
-// // }
-// // const ans = func(20);
+// // // const func= (n:any):any=>{
+// // //     return n;
+// // // }
+// // // const ans = func(20);
 
-// //generic
+// // //generic
 
-// // const func = <T>(n:T):T=>{
-// //     return n;
-// // }
-// // const ans = func(20);
-// // const ans2= func("string");
-// // const ans3 = func(true);
+// // // const func = <T>(n:T):T=>{
+// // //     return n;
+// // // }
+// // // const ans = func(20);
+// // // const ans2= func("string");
+// // // const ans3 = func(true);
 
-// // type Person = {
-// //     name :string,
-// //     age :number
-// // }
-// // const func = <T>(n:T):T=>{
-// //     return n;
-// // }
-// // const abhi:Person = {name:"Rahj",age:34}
-// // const ans = func<Person>(abhi)
-// // alert(JSON.stringify(ans))
-// const arr:number[]=[]
-// const arr2:Array<number>=[]
+// // // type Person = {
+// // //     name :string,
+// // //     age :number
+// // // }
+// // // const func = <T>(n:T):T=>{
+// // //     return n;
+// // // }
+// // // const abhi:Person = {name:"Rahj",age:34}
+// // // const ans = func<Person>(abhi)
+// // // alert(JSON.stringify(ans))
+// // const arr:number[]=[]
+// // const arr2:Array<number>=[]
 
-// const func = < T ,U>(n:T ,o:U):{n:T,o:U}=>{
-//     return {n,o};
+// // const func = < T ,U>(n:T ,o:U):{n:T,o:U}=>{
+// //     return {n,o};
+// // }
+// // // const ans = func<number,string>(20,"lol");
+// // // console.log(ans.n)
+// // type Person={
+// //     name :string,age:number
+// // }
+
+// // const func = < T , U extends T>(n:T ,o:U):{n:T,o:U}=>{
+// //     return {n,o};
+// // }
+
+// // const ans = func<Person ,{name:string,age:number,email:string}>({name:"akash",age:34},{name:"sakhal",age:23,email:"chakravartin"});
+
+
+// type Person={name:string,age:number}
+// const users:Person[]=[
+//     {name:"aghil",age:34},
+//     {name:"chakiv",age:32},
+//     {name :"sajjo",age:23},
+//     {name:"vakilsh",age:72}
+// ]
+
+// const filterByPeoples = <T,Key extends keyof T>(arr:T[],property:Key,value:T[Key]):T[]=>{
+//    return  arr.filter(item=>item[property]===value)
 // }
-// // const ans = func<number,string>(20,"lol");
-// // console.log(ans.n)
-// type Person={
-//     name :string,age:number
-// }
 
-// const func = < T , U extends T>(n:T ,o:U):{n:T,o:U}=>{
-//     return {n,o};
-// }
+// const filteredPeopleByName = filterByPeoples(users,"name","aghil")
+// const filteredPeopleByAge = filterByPeoples(users,"age",32)
 
-// const ans = func<Person ,{name:string,age:number,email:string}>({name:"akash",age:34},{name:"sakhal",age:23,email:"chakravartin"});
+// alert(JSON.stringify(filteredPeopleByName))
+// alert(JSON.stringify(filteredPeopleByAge))
 
 
-type Person={name:string,age:number}
-const users:Person[]=[
-    {name:"aghil",age:34},
-    {name:"chakiv",age:32},
-    {name :"sajjo",age:23},
-    {name:"vakilsh",age:72}
-]
-
-const filterByPeoples = <T,Key extends keyof T>(arr:T[],property:Key,value:T[Key]):T[]=>{
-   return  arr.filter(item=>item[property]===value)
-}
-
-const filteredPeopleByName = filterByPeoples(users,"name","aghil")
-const filteredPeopleByAge = filterByPeoples(users,"age",32)
-
-alert(JSON.stringify(filteredPeopleByName))
-alert(JSON.stringify(filteredPeopleByAge))
