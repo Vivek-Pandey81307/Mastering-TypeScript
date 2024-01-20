502f3aafb73262673d5efb293c98799fb425de13
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-01-20 11:17:36 +0530

    useState,FormEvent,submitHandler

--- 07 React.ts/src/App.tsx
+++ 07 React.ts/src/App.tsx
@@ -1,11 +1,29 @@
- import Box from './components/Box'
- import {useState} from 'react'
+//  import Box from './components/Box'
+ import {useState,FormEvent} from 'react'
+ interface Person{
+  name : string,
+  age:number,
+ }
 function App() {
-  const [val,setVal]=useState<string>("")
+  const [user,setUser]=useState<Person>()
+  const submitHandler =(e:FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    console.log(user);
+  }
   return (
+    // <>
+    //   {/* <div>
+    //     <Box label='Search' value={val} setter={setVal}/>
+    //   </div> */}
+    // </>
+
     <>
       <div>
-        <Box label='Search' value={val} setter={setVal}/>
+        <form onSubmit={submitHandler}>
+          <input placeholder={"Age"} type="number" value={user?.age || 0} onChange={(e)=>setUser(prev=>({...prev!,age:Number(e.target.value)}))} />
+          <input type="text" placeholder={"Name"} value={user?.name || ""} onChange={(e)=>setUser(prev=>({...prev!,name:String(e.target.value)}))} />
+          <button type='submit'>Register</button>
+        </form>
       </div>
     </>
   )
