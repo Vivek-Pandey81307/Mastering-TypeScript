ee7f113a6985a0e862c0caee91cae531a4295004
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-01-20 21:14:09 +0530

    configureStore,createAction,createReducer,addCase

--- 07 React.ts/src/App.tsx
+++ 07 React.ts/src/App.tsx
@@ -1,42 +1,9 @@
-import { useReducer } from "react";
-type StateType = {
-  count :number
-}
-type ActionType = {type :"Increment",payload :number} |
-{type : "Decrement",payload :number};
-
-const reducer = (state:StateType,action:ActionType):StateType=>{
-  switch(action.type){
-    case "Increment":
-      return {count:state.count+action.payload}
-      case "Decrement":
-        return {count:state.count-action.payload}
-    default:
-      return {count:state.count};  
-  }
-}
-const  initialState : StateType ={
-  count : 0
-}
 function App(){
-  const [state,dispatch] = useReducer(reducer,initialState)
-  const Increment = ():void=>{
-    dispatch({
-      type :'Increment',payload:1,
-    })
-  }
-  const Decrement = ():void=>{
-    dispatch({
-      type:'Decrement',payload:1
-    })
-  }
-  return <div >
-    <h1>Count Change</h1>
-    <p>Count: {state.count}</p>
-    <section style={{display:"flex",gap:10}}>
-    <button onClick={Increment}>+</button>
-    <button style={{width:'12px'}} onClick={Decrement}>-</button>
-    </section>
-  </div>
+  return (
+    <div>
+      <h1>Toolkit</h1>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  )
 }
-export default App;
