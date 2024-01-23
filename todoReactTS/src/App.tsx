c7f6236f70579ceacbcb1cfc1715359f23a7438d
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-01-23 08:37:24 +0530

    Completed

--- todoReactTS/src/App.tsx
+++ todoReactTS/src/App.tsx
@@ -2,26 +2,35 @@ import { AppBar, Container, Toolbar, Typography,Stack, TextField,Button} from "@
 import TodoItem from "./components/TodoItem"
 import { useState } from "react"
 const App = () => {
-  const [todos,setTodos] = useState<TodoItemType[]>([{title:"vikram",isCompleted:false,id:"01"},{title:"madhuma",isCompleted:false,id:"02"}])
-  
-  const completeHandler =(id:TodoItemType["id"]):void=>{alert(id)};
-  const deleteHandler = (id:TodoItemType["id"]):void=>{alert(id)}
-  return <Container maxWidth="sm" sx={{height : "100vh"}}>
-    <AppBar position="static">
-      <Toolbar>
+  const [todos,setTodos] = useState<TodoItemType[]>([])
+  const [title,setTitle] = useState<TodoItemType["title"]>("");
+  // const completeHandler =(id:TodoItemType["id"]):void=>{
+  // };
+  const deleteHandler = (id:TodoItemType["id"]):void=>{
+    setTodos((prev)=>prev.filter((todo)=>(todo.id!=id)) )
+  }
+  const eventHandler = ()=>{setTodos((prev)=>[...prev,{title:title,isCompleted:false,id:String(Math.ceil(Math.random()*10000))}]);setTitle("")}
+  const keyDownHandler=()=>{
+    setTodos((prev)=>[...prev,{title:title,isCompleted:false,id:String(Math.ceil(Math.random()*10000))}]);setTitle("");
+    
+  }
+  return <Container maxWidth="sm" sx={{height : "100vh",backgroundColor:"white" }}>
+    <br/>
+    <AppBar position="static"  sx={{borderRadius:"10px"}}>
+      <Toolbar >
         <Typography>Todo App</Typography>
       </Toolbar>
     </AppBar>
-    <Stack  height ={"70%"} direction= {"column"} spacing={"1rem"} p={"1rem"}>
+    <Stack  sx={{borderRadius:"10px"}} minHeight ={"70%"} direction= {"column"} spacing={"1rem"} p={"1rem"}>
     {todos.map((i)=>(
       <TodoItem  
       deleteHandler={deleteHandler}  
-      completeHandler={completeHandler} 
-      key={ i.id} todo={i}/>
+      
+      key={ i.id}  todo={i} />
     ))}
     </Stack>
-    <TextField fullWidth label={"NewTask"}/>
-    <Button    sx={{margin:"1rem 0"}} fullWidth variant="contained">ADD</Button>
+    <TextField  sx={{borderRadius:"10px"}} onKeyDown={(e)=>{if(e.key === 'Enter'){e.preventDefault();keyDownHandler()}}} value={title} onChange={(e)=>setTitle(e.target.value)} fullWidth label={"NewTask"}/>
+    <Button onClick={eventHandler}  sx={{margin:"1rem 0",borderRadius:"10px"}} fullWidth variant="contained">ADD</Button>
   </Container> 
 }
 
